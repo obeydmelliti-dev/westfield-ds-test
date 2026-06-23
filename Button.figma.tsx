9714067f781d50e62e4365758734942b038b8d79
obeydmelliti-dev <obeyd.melliti@gmail.com>
2026-06-23 11:14:06 +0000

    Fix props mapping

--- Button.figma.tsx
+++ Button.figma.tsx
@@ -6,16 +6,18 @@ figma.connect(
   "https://www.figma.com/design/Dty6BLOKwvt0ar1kMYEJWX/Core-Components-VF.1?node-id=2311-26545",
   {
     props: {
-      children: figma.string("Label"),
-      disabled: figma.boolean("Disabled"),
-      variant: figma.enum("Type", {
+      variant: figma.enum("type", {
         Primary: "primary",
         Secondary: "secondary",
       }),
+      disabled: figma.enum("state", {
+        Disabled: true,
+        Default: false,
+      }),
     },
-    example: ({ children, disabled, variant }) => (
+    example: ({ variant, disabled }) => (
       <Button variant={variant} disabled={disabled}>
-        {children}
+        Button
       </Button>
     ),
   }
